7092fd75414196d672c4651ab6bb92cd7c74ffd3
Jorge Aleman <aleman.waleman@gmail.com>
2025-06-20 00:37:18 -0600

    hol3

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,9 +1,9 @@
+// src/lib/supabase/server.ts
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-import type { ReadonlyRequestCookies } from 'next/dist/compiled/@edge-runtime/cookies'
 
 export const createClient = () => {
-  const cookieStore = cookies() as unknown as ReadonlyRequestCookies;
+  const cookieStore = cookies()
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -17,21 +17,17 @@ export const createClient = () => {
           try {
             cookieStore.set({ name, value, ...options })
           } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // Ignora en componentes de servidor
           }
         },
         remove(name: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value: '', ...options })
           } catch (error) {
-            // The `delete` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // Ignora en componentes de servidor
           }
-        },
-      },
+        }
+      }
     }
   )
-} 
+}
